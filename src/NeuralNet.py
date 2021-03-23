e127371bcbe71b968ab27da1f8824cb4e65f3d76
Rohith04MVK <007rohitjj04@gmail.com>
2021-03-23 09:48:54 +0000

    Initialize the weights and biases

--- src/NeuralNet.py
+++ src/NeuralNet.py
@@ -5,3 +5,21 @@ class NN:
         self.shape = shape
         self.neuron = neuron
         self.layers = layers
+
+    def init_layers(nn_architecture, seed = 99):
+        np.random.seed(seed)
+        number_of_layers = len(nn_architecture)
+        params_values = {}
+
+        for idx, layer in enumerate(nn_architecture):
+            layer_idx = idx + 1
+            layer_input_size = layer["input_dim"]
+            layer_output_size = layer["output_dim"]
+            
+            params_values['W' + str(layer_idx)] = np.random.randn(
+                layer_output_size, layer_input_size) * 0.1
+            params_values['b' + str(layer_idx)] = np.random.randn(
+                layer_output_size, 1) * 0.1
+            
+        return params_values
+
