41b4dcfd4e76e2283d7e90e18110b5585641bc41
Rohith04MVK <007rohitjj04@gmail.com>
2021-03-28 09:25:09 +0000

    Added the derivative of sigmoid and relu function

--- src/NeuralNet.py
+++ src/NeuralNet.py
@@ -7,27 +7,34 @@ class NN:
         self.layers = layers
 
     def init_layers(nn_architecture, seed = 99):
+        # random seed initiation
         np.random.seed(seed)
+        # number of layers in our neural network
         number_of_layers = len(nn_architecture)
+        # parameters storage initiation
         params_values = {}
-
+        
+        # iteration over network layers
         for idx, layer in enumerate(nn_architecture):
+            # we number network layers from 1
             layer_idx = idx + 1
+            
+            # extracting the number of units in layers
             layer_input_size = layer["input_dim"]
             layer_output_size = layer["output_dim"]
             
+            # initiating the values of the W matrix
+            # and vector b for subsequent layers
             params_values['W' + str(layer_idx)] = np.random.randn(
                 layer_output_size, layer_input_size) * 0.1
             params_values['b' + str(layer_idx)] = np.random.randn(
                 layer_output_size, 1) * 0.1
+        
+    return params_values
             
-        return params_values
+    def sigmoid(Z):
+        return 1/(1+np.exp(-Z))
 
+    def relu(Z):
+        return np.maximum(0,Z)
 
-    def ReLU(Z):
-    return np.maximum(Z, 0)
-
-
-    def softmax(Z):
-        A = np.exp(Z) / sum(np.exp(Z))
-        return A
