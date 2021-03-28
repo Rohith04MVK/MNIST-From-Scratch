f5f05a62583c9422c7cd97965e58db78e62fdf04
Rohith04MVK <007rohitjj04@gmail.com>
2021-03-28 09:38:00 +0000

    Added full backward propagation

--- src/NeuralNet.py
+++ src/NeuralNet.py
@@ -137,3 +137,36 @@ class NN:
         dA_prev = np.dot(W_curr.T, dZ_curr)
 
         return dA_prev, dW_curr, db_curr
+
+    def full_backward_propagation(Y_hat, Y, memory, params_values, nn_architecture):
+        grads_values = {}
+        
+        # number of examples
+        m = Y.shape[1]
+        # a hack ensuring the same shape of the prediction vector and labels vector
+        Y = Y.reshape(Y_hat.shape)
+        
+        # initiation of gradient descent algorithm
+        dA_prev = - (np.divide(Y, Y_hat) - np.divide(1 - Y, 1 - Y_hat));
+        
+        for layer_idx_prev, layer in reversed(list(enumerate(nn_architecture))):
+            # we number network layers from 1
+            layer_idx_curr = layer_idx_prev + 1
+            # extraction of the activation function for the current layer
+            activ_function_curr = layer["activation"]
+            
+            dA_curr = dA_prev
+            
+            A_prev = memory["A" + str(layer_idx_prev)]
+            Z_curr = memory["Z" + str(layer_idx_curr)]
+            
+            W_curr = params_values["W" + str(layer_idx_curr)]
+            b_curr = params_values["b" + str(layer_idx_curr)]
+            
+            dA_prev, dW_curr, db_curr = single_layer_backward_propagation(
+                dA_curr, W_curr, b_curr, Z_curr, A_prev, activ_function_curr)
+            
+            grads_values["dW" + str(layer_idx_curr)] = dW_curr
+            grads_values["db" + str(layer_idx_curr)] = db_curr
+        
+        return grads_values
