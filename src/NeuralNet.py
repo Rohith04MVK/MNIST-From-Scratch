2f8875db2336673ff832eaa3a166ed93af73a558
Rohith04MVK <007rohitjj04@gmail.com>
2021-03-28 09:29:24 +0000

    Added single layer forward propagation

--- src/NeuralNet.py
+++ src/NeuralNet.py
@@ -39,4 +39,17 @@ class NN:
         dZ[Z <= 0] = 0
         return dZ
 
-
+    def single_layer_forward_propagation(A_prev, W_curr, b_curr, activation="relu"):
+        # calculation of the input value for the activation function
+        Z_curr = np.dot(W_curr, A_prev) + b_curr
+        
+        # selection of activation function
+        if activation is "relu":
+            activation_func = relu
+        elif activation is "sigmoid":
+            activation_func = sigmoid
+        else:
+            raise Exception('Non-supported activation function')
+            
+        # return of calculated activation A and the intermediate Z matrix
+        return activation_func(Z_curr), Z_curr
